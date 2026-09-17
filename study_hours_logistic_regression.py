118a561a47f5ccda7b53157222bfe1b3652f9262
csdiyafathima <csdiyafathima@gmail.com>
2026-09-17 11:29:34 +0530

    Update study_hours_logistic_regression.py

--- study_hours_logistic_regression.py
+++ study_hours_logistic_regression.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import joblib
-model=joblib.load("logistic_regression_student_studyhours_model.pkl")
+model=joblib.load("logistic_regression_student_studyhours_model(2).pkl")
 st.title("🎓 Student Pass Prediction")
-st.write("Enter the student's study hours to predict the result.")
+st.write("Enter the student's study hours  and attendance to predict the result.")
 
 hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=24.0,value=5.0,step=0.5)
+attendance=st.number_input("Enter the Attendance",min_value=0.0,max_value=100,value=69.0,step=1.0)
 if st.button("🔮 Predict Result"):
-    input_data = [[hours]]
+    input_data = [[hours,attendance]]
     prediction = model.predict(input_data)
     probabilities = model.predict_proba(input_data)
     pass_probability = probabilities[0][1] * 100
@@ -19,7 +20,14 @@ if st.button("🔮 Predict Result"):
     else:
         st.error("❌ Student is predicted to FAIL")
 
-    st.write(f"Probability of Passing: {pass_probability:.2f}%")
+    st.metric(
+        "🎯 Probability of Passing",
+        f"{pass_probability:.2f}%"
+    )
+
     st.progress(pass_probability / 100)
 
-    st.write(f"Probability of Failing: {fail_probability:.2f}%")
+    
+    st.write(
+        f"❌ Probability of Failing: **{fail_probability:.2f}%**"
+    )
